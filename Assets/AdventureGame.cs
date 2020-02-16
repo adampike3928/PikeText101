57d3657fa680ee9c6b9a4b596c76436cb0b1d7c8
adampike334 <adampikespam@gmail.com>
2020-02-15 20:16:05 -0500

    Manage Next States

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -18,7 +19,22 @@ public class AdventureGame : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {//checks managestate for updates
+        ManageState();
     }
+
+    private void ManageState()
+    {//returns an array
+        var nextStates = state.GetNextStates();
+        //Input for change of state
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[2];
+        }
+        txtcomponent.text = state.GetStateStory();
+    }   
 }
